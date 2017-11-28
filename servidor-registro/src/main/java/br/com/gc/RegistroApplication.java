18b56159735744b088e53468a507f580619d8932
anderson.silva <infoander@gmail.com>
2017-11-28 16:58:48 -0200

    Serviço de registrar as apis Euricka

--- servidor-registro/src/main/java/br/com/gc/RegistroApplication.java
+++ servidor-registro/src/main/java/br/com/gc/RegistroApplication.java
@@ -1,13 +1,13 @@
-package gc;
+package br.com.gc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaServer
 public class RegistroApplication {
     public static void main(String args[]) {
-        SpringApplication.run(ConfiguracaoApplication.class, args);
+        SpringApplication.run(RegistroApplication.class, args);
     }
 }
